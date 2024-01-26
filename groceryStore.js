1a8f9b974afc27139cf4ca50adbd2f1c1fdad786
KheswariG <kgukh078@uottawa.ca>
2024-01-26 15:39:44 -0500

    second pushh

---
+++ groceryStore.js
@@ -0,0 +1,6 @@
+function openMenu(evt, tabName){
+    //getting all elements from class="tabcontent" and hiding them
+    for (i= 0, i<tabcontent.length;i++){
+        tabcontent[i].style.display="none";
+    }
+}
